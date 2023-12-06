a102c0b8672930408afe1557cda57623d925fbf3
Lívia Raiane <liviaraianen@gmail.com>
2023-12-06 15:51:54 -0300

    feat: estrutura inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDocs.GettingStarted;
+
+public static class FirstScript
+{
+    public static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("https://www.google.com");
+
+        var title = driver.Title;
+
+        Console.WriteLine(title);
+            
+        driver.Quit();
+    }
+}
